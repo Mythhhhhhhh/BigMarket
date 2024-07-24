929f2b4bdaee6188b5544836a19f1edac524ec94
theoden <574630686@qq.com>
2024-07-24 22:23:27 +0800

    feat:对接联调额度签到权重接口

--- BigMarket-app/build.sh
+++ BigMarket-app/build.sh
@@ -1,6 +1,6 @@
 
 # 普通镜像构建，随系统版本构建 amd/arm
-docker build -t myth/big-market-app:1.0 -f ./Dockerfile .
+docker build -t myth/big-market-app:2.0 -f ./Dockerfile .
 
 # 兼容 amd、arm 构建镜像
 #docker buildx build --load --platform liunx/amd64,linux/arm64 -t myth/big-market-app:1.0-SNAPSHOT -f ./Dockerfile . --push
