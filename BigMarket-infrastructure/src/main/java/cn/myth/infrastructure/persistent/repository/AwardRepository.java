e2021059bc1cc03bb14b5132ff2c32a1430dff05
theoden <574630686@qq.com>
2024-07-26 00:37:23 +0800

    fix:增加 redis 锁，和账户修改前的查询判断，避免产生间隙锁问题。

--- BigMarket-infrastructure/src/main/java/cn/myth/infrastructure/persistent/repository/AwardRepository.java
+++ BigMarket-infrastructure/src/main/java/cn/myth/infrastructure/persistent/repository/AwardRepository.java
@@ -14,15 +14,19 @@ import cn.myth.infrastructure.persistent.po.Task;
 import cn.myth.infrastructure.persistent.po.UserAwardRecord;
 import cn.myth.infrastructure.persistent.po.UserCreditAccount;
 import cn.myth.infrastructure.persistent.po.UserRaffleOrder;
+import cn.myth.infrastructure.persistent.redis.IRedisService;
+import cn.myth.types.common.Constants;
 import cn.myth.types.enums.ResponseCode;
 import cn.myth.types.exception.AppException;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 奖品仓储服务
@@ -47,6 +51,8 @@ public class AwardRepository implements IAwardRepository {
     private IUserRaffleOrderDao userRaffleOrderDao;
     @Resource
     private IUserCreditAccountDao userCreditAccountDao;
+    @Resource
+    private IRedisService redisService;
 
     @Override
     public void saveUserAwardRecord(UserAwardRecordAggregate userAwardRecordAggregate) {
@@ -139,14 +145,17 @@ public class AwardRepository implements IAwardRepository {
         userCreditAccountReq.setAvailableAmount(userCreditAwardEntity.getCreditAmount());
         userCreditAccountReq.setAccountStatus(AccountStatusVO.open.getCode());
 
+        RLock lock = redisService.getLock(Constants.RedisKey.ACTIVITY_ACCOUNT_LOCK + userId);
         try {
+            lock.lock(3, TimeUnit.SECONDS);
             dbRouter.doRouter(giveOutPrizesAggregate.getUserId());
             transactionTemplate.execute(status -> {
                 try {
-                    // 更新积分 || 创建积分账户
-                    int updateAccountCount = userCreditAccountDao.updateAddAmount(userCreditAccountReq);
-                    if (0 == updateAccountCount) {
+                    UserCreditAccount userCreditAccountRes = userCreditAccountDao.queryUserCreditAccount(userCreditAccountReq);
+                    if (null == userCreditAccountRes) {
                         userCreditAccountDao.insert(userCreditAccountReq);
+                    } else {
+                        userCreditAccountDao.updateAddAmount(userCreditAccountReq);
                     }
 
                     // 更新奖品记录
